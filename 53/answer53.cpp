d3dcc8f603b461ce1cf0ecec50648dde73cb3058
fdkeylabhu <44796297+fdkeylabhu@users.noreply.github.com>
2019-11-30 11:01:01 +0800

    Update answer53.cpp

--- 53/answer53.cpp
+++ 53/answer53.cpp
@@ -1,11 +1,20 @@
 
+/****************************************
+sum 保存当前累加和
+1. 当前的值nums[i]对累加和有增益， sum+nums[i]
+2.当前值对累加和负增益，则sum=nums[i]，从该位置计算当前累加和
 
+每次循环与最大累加和进行比较
+1. 如果sum > 最大累加和ans
+  ans = sum
+2. 否则ans保持不变
+****************************************/
 
 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = nums[0];
+        int ans = nums[0];
         int sum = 0;
         const int len = nums.size();     
         
@@ -15,10 +24,11 @@ public:
             } else {
                 sum += nums[i];
             }            
-            max_sum = max_sum > sum? max_sum:sum;
-        }
-        
-        
-        return max_sum;
+            
+            if (sum > ans) {   //当前累加和 > 最大累加和？
+                ans = sum;
+            }
+        }     
+        return ans;
     }
 };
