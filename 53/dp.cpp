35760891ffdd49f4e51b6f11128044992a60715e
fdkeylabhu <44796297+fdkeylabhu@users.noreply.github.com>
2019-11-30 11:12:30 +0800

    Update dp.cpp

--- 53/dp.cpp
+++ 53/dp.cpp
@@ -1,24 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        const int len = nums.size();
-        vector<int> acc(nums);         //保存第i位置最大累加和
-        for (int i = 0; i < len; ++i) {
-            sum += nums[i];
-            if (sum < 0) {
-                sum = 0;
-            } else {
-                acc[i] = sum;
-            }
+    int maxSubArray(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int len = nums.size();
+        //dp[i]表示nums中以nums[i]结尾的最大子序和
+        vector<int> dp(len);
+        dp[0] = nums[0];
+        ans = dp[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);      //dp[i]的值取决于dp[i-1]与nums[i]的关系
         }
-        int max = INT_MIN;
-        for (int i = 0; i < len; ++i) {
-            if (acc[i] > max) {
-                max = acc[i];
-            }
-        }
-        
-        return max;
+        ans = *max_element(dp.begin(),dp.end());
+        return ans;
     }
 };
